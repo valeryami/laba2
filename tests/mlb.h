7d0953fbf2e425eea7a8adb46b18ab4d01ed9aa4
valeryami <78733965+valeryami@users.noreply.github.com>
2022-10-21 08:28:56 +0300

    Update mlb.h

--- tests/mlb.h
+++ tests/mlb.h
@@ -8,11 +8,14 @@ TEST(mlb, on_first_begin)
     std::string output_text = "|";
 
     text txt = create_text();
-    for (int i = 0; i < 10; i++) {
-        append_line(txt, "mlb test " + std::to_string(i));
-        output_text += "mlb test " + std::to_string(i) + "\n";
-    }
-
+    
+    append_line(txt, "milka");
+    output_text += "milka\n";
+    append_line(txt, "milka");
+    output_text += "milka\n";
+    append_line(txt, "milka");
+    output_text += "milka\n";
+    
     testing::internal::CaptureStdout();
 
     m(txt, 0, 0);
@@ -27,39 +30,43 @@ TEST(mlb, on_first)
 {
     std::string output_text = "|";
 
+    
+    
     text txt = create_text();
-    for (int i = 0; i < 10; i++) {
-        append_line(txt, "mlb test " + std::to_string(i));
-        output_text += "mlb test " + std::to_string(i) + "\n";
-    }
-
+    
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    
     testing::internal::CaptureStdout();
 
-    m(txt, 0, 10);
+    m(txt, 0, 5);
     mlb(txt);
     show(txt);
-    std::string text = testing::internal::GetCapturedStdout();
+    std::string text = testing::internal::GeCapturedStdout();
 
     EXPECT_STREQ(text.c_str(), output_text.c_str());
 }
 
-TEST(mlb, on_center_begin)
+TEST(mlb, on_center)
 {
     std::string output_text = "";
 
     text txt = create_text();
-    for (int i = 0; i < 10; i++) {
-        append_line(txt, "mlb test " + std::to_string(i));
-        if (i == 4) {
-            output_text += "|mlb test " + std::to_string(i) + "\n";
-        } else {
-            output_text += "mlb test " + std::to_string(i) + "\n";
-        }
-    }
+    
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "|milka and poilka\n";
 
     testing::internal::CaptureStdout();
 
-    m(txt, 4, 0);
+    m(txt, 2, 0);
     mlb(txt);
     show(txt);
     std::string text = testing::internal::GetCapturedStdout();
@@ -72,16 +79,22 @@ TEST(mlb, on_center)
 {
     std::string output_text = "";
 
+    
     text txt = create_text();
-    for (int i = 0; i < 10; i++) {
-        append_line(txt, "mlb test " + std::to_string(i));
-        if (i == 4) {
-            output_text += "|mlb test " + std::to_string(i) + "\n";
-        } else {
-            output_text += "mlb test " + std::to_string(i) + "\n";
-        }
-    }
-
+    
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "|milka and poilka\n";
+    append_line(txt, "milka and poilka");
+    output_text += "milka and poilka\n";
+   
     testing::internal::CaptureStdout();
 
     m(txt, 4, 8);
@@ -91,53 +104,4 @@ TEST(mlb, on_center)
 
     EXPECT_STREQ(text.c_str(), output_text.c_str());
 }
-
-TEST(mlb, on_last_begin)
-{
-    std::string output_text = "";
-
-    text txt = create_text();
-    for (int i = 0; i < 10; i++) {
-        append_line(txt, "mlb test " + std::to_string(i));
-        if (i == 9) {
-            output_text += "|mlb test " + std::to_string(i) + "\n";
-        } else {
-            output_text += "mlb test " + std::to_string(i) + "\n";
-        }
-    }
-
-    testing::internal::CaptureStdout();
-
-    m(txt, 9, 0);
-    mlb(txt);
-    show(txt);
-    std::string text = testing::internal::GetCapturedStdout();
-
-    EXPECT_STREQ(text.c_str(), output_text.c_str());
-}
-
-TEST(mlb, on_last)
-{
-    std::string output_text = "";
-
-    text txt = create_text();
-    for (int i = 0; i < 10; i++) {
-        append_line(txt, "mlb test " + std::to_string(i));
-        if (i == 9) {
-            output_text += "|mlb test " + std::to_string(i) + "\n";
-        } else {
-            output_text += "mlb test " + std::to_string(i) + "\n";
-        }
-    }
-
-    testing::internal::CaptureStdout();
-
-    m(txt, 9, 8);
-    mlb(txt);
-    show(txt);
-    std::string text = testing::internal::GetCapturedStdout();
-
-    EXPECT_STREQ(text.c_str(), output_text.c_str());
-}
-
 #endif // MLB_H
