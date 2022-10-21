e98dbd259386f3d3f1888b6863fc74c363f218fa
valeryami <78733965+valeryami@users.noreply.github.com>
2022-10-21 07:10:58 +0300

    Update remove_all.h

--- tests/remove_all.h
+++ tests/remove_all.h
@@ -1,1 +1,24 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include <string>
 
+#include "common.h"
+#include "text.h"
+#include "_text.h"
+
+using namespace std;
+
+TEST(delite, positive)
+{
+  
+    std::string output_text = "The text doesn't exist!\n";
+
+    testing::internal::CaptureStdout();
+
+    string txt;
+    remove_all(txt)
+    
+    std::string text = testing::internal::GetCapturedStdout();
+
+    EXPECT_STREQ(text.c_str(), output_text.c_str());
+}
